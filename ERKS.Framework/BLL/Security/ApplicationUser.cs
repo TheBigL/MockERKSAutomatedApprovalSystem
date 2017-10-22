d13b20b07db0aef588d5be5397fc3e3e54e7de5c
TheBigL <lmohamed1@studentmail.nait.ca>
2017-10-22 14:59:31 -0600

    Setting up Security Part 1

---
+++ ERKS.Framework/BLL/Security/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERKS.Framework.BLL.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int AdminId { get; set; }
+        public int StaffId { get; set; }
+
+    }
+}
---
+++ ERKS.Framework/BLL/Security/SecurityRoles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERKS.Framework.BLL.Security
+{
+    internal static class SecurityRoles
+    {
+        public const string WebAdmins = "WebAdmins";
+        public const string Staff = "Staff";
+
+
+        public static List<string> StartUpSecurityRoles
+        {
+            get
+            {
+                List<string> value = new List<string>();
+                value.Add(WebAdmins);
+                value.Add(Staff);
+                return value;
+            }
+        }
+    }
+}
