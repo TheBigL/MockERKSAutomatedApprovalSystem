4c28b89c08f988b71835f12185745416d329db56
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-23 16:27:10 -0600

    Entity LLD_ATS
    Create LLD_ATS table in the entities folder

---
+++ ERKS.Framework/Entities/LLD_ATS.cs
@@ -0,0 +1,23 @@
+﻿namespace ERKS.Framework.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public partial class LLD_ATS
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+
+        public int ATSID { get; set; }
+        public int MeridianNumber { get; set; }
+        public int RangeNumber { get; set; }
+        public int TownshipNumber { get; set; }
+        public int SectionNumber { get; set; }
+        public int QuarterSectionNumber { get; set; }
+        public int LSD { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+    }
+}
