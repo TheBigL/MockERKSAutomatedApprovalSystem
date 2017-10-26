740f2ce209afb96df01c8e4a8b2348fae7230946
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:15:11 -0600

    Entity SPIN_II

---
+++ ERKS.Framework/Entities/SPIN_II.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("SPIN_II")]
+    public partial class SPIN_II
+    {
+        public int LINC_Number { get; set; }
+
+        public string Street_Range { get; set; }
+
+        public string Avenue_Range { get; set; }
+    }
+}
