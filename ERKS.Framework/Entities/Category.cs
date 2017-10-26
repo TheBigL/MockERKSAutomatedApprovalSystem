11e7201ef0e465a484afe93394b09e5c4d92c693
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:06:22 -0600

    Entity Category
    Create Category entity

---
+++ ERKS.Framework/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Category")]
+    public partial class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
--- ERKS.Framework/Entities/LLD_ATS.cs
+++ ERKS.Framework/Entities/LLD_ATS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ERKS.Framework.Entities
 {
     [Table("LLD_ATS")]
