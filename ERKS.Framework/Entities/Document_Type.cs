e432d6bd0942391250fd915cc8a0e4bb35c75a92
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:27:37 -0600

    Entity Document_Type

---
+++ ERKS.Framework/Entities/Document_Type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Document_Type")]
+   public partial class Document_Type
+    {
+        public int Document_Type_ID { get; set; }
+
+        public string Document_Type_Description { get; set; }
+
+        public DateTime? From_Date { get; set; }
+
+        public int Microfilm_Roll_Number { get; set; }
+
+        public int Microfilm_Frame_Number { get; set; }
+    }
+}
