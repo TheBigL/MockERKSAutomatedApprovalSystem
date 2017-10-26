0176b7c2b10772118bc6ffba3e86efce9cb0cf5f
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 13:56:40 -0600

    Correct LLD_ATS

--- ERKS.Framework/Entities/LLD_ATS.cs
+++ ERKS.Framework/Entities/LLD_ATS.cs
@@ -1,23 +1,28 @@
-﻿namespace ERKS.Framework.Entities
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ERKS.Framework.Entities
+{
+    [Table("LLD_ATS")]
     public partial class LLD_ATS
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
 
         public int ATSID { get; set; }
+
         public int MeridianNumber { get; set; }
+
         public int RangeNumber { get; set; }
+
         public int TownshipNumber { get; set; }
+
         public int SectionNumber { get; set; }
+
         public int QuarterSectionNumber { get; set; }
+
         public int LSD { get; set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-
+        
     }
 }
