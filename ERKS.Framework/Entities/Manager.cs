332ac6231d7097e19933160e0980e0109cddcf7b
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:19:57 -0600

    Entity Manager

---
+++ ERKS.Framework/Entities/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Manager")]
+    public partial class Manager
+    {
+        public int Manager_ID { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
