bef2638ae988f932d3092ce66cf8f1fe02d550b1
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:24:14 -0600

    Entity Security_Classification

---
+++ ERKS.Framework/Entities/Security_Classification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Security_Classification")]
+    public partial class Security_Classification
+    {
+        public int Security_Classification_ID { get; set; }
+
+        public string Security_Classification_Name { get; set; }
+
+        public string Security_Classification_Description { get; set; }
+    }
+}
