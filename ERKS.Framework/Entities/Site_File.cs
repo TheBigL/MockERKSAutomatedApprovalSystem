490fede381ce031748e7f76e0cf85ee0b4da4c56
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:40:26 -0600

    Entity Site_File

---
+++ ERKS.Framework/Entities/Site_File.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Site_File")]
+    public partial class Site_File
+    {
+        public int File_ID { get; set; }
+
+        public int Type_ID { get; set; }
+
+        public int Operation_ID { get; set; }
+
+        public int PBL_ID { get; set; }
+
+        public int Category_ID { get; set; }
+
+        public int LINC_Number { get; set; }
+
+        public int Organization_ID { get; set; }
+
+        public int Document_Type_ID { get; set; }
+
+        public int Security_Classification_ID { get; set; }
+
+        public string File_Status { get; set; }
+
+        public DateTime? Closed_Date { get; set; }
+    }
+}
