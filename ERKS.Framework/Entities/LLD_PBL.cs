0fa01df46c87c4f8dee1cf65d7b5e11883b9bcd9
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:12:05 -0600

    Create Entity LLD_PBL

---
+++ ERKS.Framework/Entities/LLD_PBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("LLD_PBL")]
+    public partial class LLD_PBL
+    {
+        public int PBL_ID { get; set; }
+
+        public int Plan_Number { get; set; }
+
+        public int Block_Number { get; set; }
+
+        public int Lot_Number { get; set; }
+    }
+}
