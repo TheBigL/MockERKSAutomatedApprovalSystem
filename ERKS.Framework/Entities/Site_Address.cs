4ded14c93796b0e989f9433592398914603ce642
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:33:08 -0600

    Entity Site_Address

---
+++ ERKS.Framework/Entities/Site_Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Site_Address")]
+    public partial class Site_Address
+    {
+        public int Location_Code { get; set; }
+
+        public int ATS_ID { get; set; }
+
+        public string Location { get; set; }
+
+        public string Address { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string Postal_Code { set; set; }
+    }
+}
