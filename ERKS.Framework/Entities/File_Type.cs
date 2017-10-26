f7b7e37da55b0520e2c5824a117c4a55fc8872ae
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:30:10 -0600

    Entity File_Type

---
+++ ERKS.Framework/Entities/File_Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("File_Type")]
+    public partial class File_Type
+    {
+        public int Type_ID { get; set; }
+
+        public string Type_Description { get; set; }
+    }
+}
