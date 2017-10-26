e8d46fa46e9c4e3f8b8f0f69c3b5b7ec43650ea3
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:22:01 -0600

    Entity Officer

---
+++ ERKS.Framework/Entities/Officer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Officer")]
+    public partial class Officer
+    {
+        public int Officer_ID { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
