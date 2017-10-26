8d3d0fe854a9a34a053253d35038ea022008268d
wzhang28 <wzhang28@studentmail.nait.ca>
2017-10-26 14:55:43 -0600

    Entity Manager_Group

---
+++ ERKS.Framework/Entities/Manager_Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERKS.Framework.Entities
+{
+    [Table("Manager_Group")]
+    public partial class Manager_Group
+    {
+        public int Manager_Group_ID { get; set; }
+
+        public int Manager_ID { get; set; }
+
+        public string Manager_Group_Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
