1f9503d90af75779d465abf21c7ca97a114f6a72
squadri2 <sayedquadri91@hotmail.com>
2017-10-30 15:28:26 -0600

    Roslyn , web config

--- ERKS.Framework/DAL/MockERKSContext.cs
+++ ERKS.Framework/DAL/MockERKSContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+
+using ERKS.Framework.Entities;
 
 namespace ERKS.Framework.DAL
 {
-    class MockERKSContext
+    public class MockERKSContext : DbContext
     {
+        public MockERKSContext()
+            : base("name=MockERKS")
+        {
+        }
+
     }
 }
